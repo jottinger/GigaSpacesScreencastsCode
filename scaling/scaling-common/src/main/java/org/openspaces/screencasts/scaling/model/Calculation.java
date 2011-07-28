1623da0706572c091d2b5906317680ef61916c24
U-osiris\joeo <joeo@osiris.(none)>
2011-07-28 07:50:33 -0400

    Adding event type so we can process events in scaling-event

---
+++ scaling/scaling-common/src/main/java/org/openspaces/screencasts/scaling/model/Calculation.java
@@ -0,0 +1,95 @@
+package org.openspaces.screencasts.scaling.model;
+
+import com.gigaspaces.annotation.pojo.SpaceClass;
+import com.gigaspaces.annotation.pojo.SpaceDynamicProperties;
+import com.gigaspaces.annotation.pojo.SpaceId;
+import com.gigaspaces.annotation.pojo.SpaceProperty;
+
+import java.math.BigInteger;
+import java.util.Set;
+
+@SpaceClass
+public class Calculation {
+    String id;
+    BigInteger source;
+    Set<BigInteger> factors;
+    Boolean processed;
+
+    public Calculation() {
+    }
+
+    public Calculation(BigInteger source) {
+        this.source = source;
+    }
+
+    @SpaceDynamicProperties
+    public Set<BigInteger> getFactors() {
+        return factors;
+    }
+
+    public void setFactors(Set<BigInteger> factors) {
+        this.factors = factors;
+    }
+
+    @SpaceId(autoGenerate = true)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @SpaceProperty
+    public BigInteger getSource() {
+        return source;
+    }
+
+    public void setSource(BigInteger source) {
+        this.source = source;
+    }
+
+    public Boolean getProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(Boolean processed) {
+        this.processed = processed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Calculation)) return false;
+
+        Calculation that = (Calculation) o;
+
+        if (factors != null ? !factors.equals(that.factors) : that.factors != null) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (processed != null ? !processed.equals(that.processed) : that.processed != null) return false;
+        if (source != null ? !source.equals(that.source) : that.source != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (source != null ? source.hashCode() : 0);
+        result = 31 * result + (factors != null ? factors.hashCode() : 0);
+        result = 31 * result + (processed != null ? processed.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("Calculation{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", source=").append(source);
+        sb.append(", processed=").append(processed);
+        sb.append(",factors=").append(factors);
+        sb.append('}');
+        return sb.toString();
+    }
+}
